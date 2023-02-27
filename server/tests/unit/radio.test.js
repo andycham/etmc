eaab839e0bcc4a672f905354e185a19bb8adcb2a
Andy Cham <andrew.cham@gmail.com>
2023-02-27 23:27:29 +0000

    Test that robot objects are created from Radio input.

--- server/tests/unit/radio.test.js
+++ server/tests/unit/radio.test.js
@@ -21,3 +21,37 @@ test('Can accept input and provide correct output.', () => {
   radio.input("5 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL");
   expect(radio.output).toBe("1 1 E\n3 3 N LOST\n2 3 S");
 });
+
+test("Can create planet from first line of input.", () => {
+  const radio = new Radio();
+  radio.input("5 3\n1 1 E\nLR\n3 2 N\nLR\n0 3 W\nLR");  // Robots just turning without going forward.
+  expect(radio.planet.maxX).toBe(5);
+  expect(radio.planet.maxY).toBe(3);
+});
+
+test("Can create robots from input.", () => {
+  const radio = new Radio();
+  radio.input("5 3\n1 2 E\nLR\n3 4 N\nLR\n0 3 W\nLR");  // Robots just turning without going forward.
+  expect(radio.robots.length).toBe(3);  
+});
+
+test("Can create first robot from input.", () => {
+  const radio = new Radio();
+  radio.input("5 3\n1 2 E\nLR\n3 4 N\nLR\n0 3 W\nLR");  // Robots just turning without going forward.
+  expect(radio.robots[0].posX).toBe(1);
+  expect(radio.robots[0].posY).toBe(2);  
+});
+
+test("Can create second robot from input.", () => {
+  const radio = new Radio();
+  radio.input("5 3\n1 2 E\nLR\n3 4 N\nLR\n0 3 W\nLR");  // Robots just turning without going forward.
+  expect(radio.robots[1].posX).toBe(3);
+  expect(radio.robots[1].posY).toBe(4);  
+});
+
+test("Can create third robot from input.", () => {
+  const radio = new Radio();
+  radio.input("5 3\n1 2 E\nLR\n3 4 N\nLR\n0 3 W\nLR");  // Robots just turning without going forward.
+  expect(radio.robots[2].posX).toBe(0);
+  expect(radio.robots[2].posY).toBe(3);  
+});
