0131075cdcac136cd4720fe83f8532b859815a8f
Andy Cham <andrew.cham@gmail.com>
2023-02-27 22:14:02 +0000

    Amend unit test to include maxX and maxY parameters for robot.

--- server/tests/unit/robot.test.js
+++ server/tests/unit/robot.test.js
@@ -1,7 +1,9 @@
 const Robot = require('../../src/models/Robot');
 
 test('Can create a Robot object', () => {
-  const robot = new Robot(1, 2, "N");
+  const robot = new Robot(5, 3, 1, 2, "N");
+  expect(robot.maxX).toBe(5);
+  expect(robot.maxY).toBe(3);
   expect(robot.posX).toBe(1);
   expect(robot.posY).toBe(2);
   expect(robot.orientation).toBe("N");
