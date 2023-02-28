047bdc1f3aed44494805a2acc2e83556c1760e2d
Andy Cham <andrew.cham@gmail.com>
2023-02-28 03:03:35 +0000

    Added test for Robot does not move if in same position as previously lost robot.

--- server/tests/unit/robot.test.js
+++ server/tests/unit/robot.test.js
@@ -8,3 +8,12 @@ test('Can create a Robot object', () => {
   expect(robot.posY).toBe(2);
   expect(robot.orientation).toBe("N");
 });
+
+test("Robot does not move if in same position as previously lost robot.", () => {
+  const robot = new Robot(5, 3, 1, 2, "N");
+  robot.lostCoordsList([1,3,"N"]);
+  robot.executeCommand("FF");  
+  expect(robot.posX).toBe(1);
+  expect(robot.posY).toBe(3);  
+  expect(robot.output()).toBe("1 3 N");
+});
