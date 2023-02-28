e75dceb3af0ad3828137137062daff305a8c916f
Andy Cham <andrew.cham@gmail.com>
2023-02-28 03:20:19 +0000

    Added test for isLostCoords.

--- server/tests/unit/robot.test.js
+++ server/tests/unit/robot.test.js
@@ -9,16 +9,17 @@ test('Can create a Robot object', () => {
   expect(robot.orientation).toBe("N");
 });
 
-test("isLostCoords returns true if current robot position is in lostCoordsList"), () => {
+test("isLostCoords returns true if current robot position is in lostCoordsList", () => {
   const robot = new Robot(5, 3, 1, 2, "N");
-  robot.lostCoordsList([1,3,"N"]);
+  robot.lostCoordsList = [[1,3,"N"]];
   expect(robot.isLostCoords()).toBe(true);  
-}
+});
 
 test("Robot does not move if in same position as previously lost robot.", () => {
   const robot = new Robot(5, 3, 1, 2, "N");
-  robot.lostCoordsList([1,3,"N"]);
-  robot.executeCommand("FF");  
+  robot.lostCoordsList = [[1,3,"N"]];
+  robot.executeCommand("F");  
+  robot.executeCommand("F");
   expect(robot.posX).toBe(1);
   expect(robot.posY).toBe(3);  
   expect(robot.output()).toBe("1 3 N");
