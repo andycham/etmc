422d77f074a17ad29b8893e95b9f590915d6e380
Andy Cham <andrew.cham@gmail.com>
2023-02-28 04:12:51 +0000

    Add test for error when coordinates are over 50.

--- server/tests/unit/robot.test.js
+++ server/tests/unit/robot.test.js
@@ -24,3 +24,20 @@ test("Robot does not move if in same position as previously lost robot.", () => 
   expect(robot.posY).toBe(3);  
   expect(robot.output()).toBe("1 3 N");
 });
+
+
+test("Throw error if X coordinate is over 50.", () => {
+  expect(() => {new Robot(5, 3, 51, 20, "N")}).toThrow();
+});
+
+test("Throw error if Y coordinate is over 50.", () => {
+  expect(() => {new Robot(5, 3, 1, 51, "N")}).toThrow();
+});
+
+test("Throw error if Max X coordinate is over 50.", () => {
+  expect(() => {new Robot(51, 3, 1, 20, "N")}).toThrow();
+});
+
+test("Throw error if Max Y coordinate is over 50.", () => {
+  expect(() => {new Robot(5, 51, 1, 20, "N")}).toThrow();
+});
